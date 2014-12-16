cd12f75dbb560cbf56981d1a5e1812f7fb47134a
Julien Gomès <Julien Goms>
2014-12-16 17:00:06 +0100

    Update iOS version

--- TAL/TAL.iOS/HomeViewController.cs
+++ TAL/TAL.iOS/HomeViewController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Drawing;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
 
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
@@ -14,6 +16,11 @@ namespace TAL.iOS
 	public partial class HomeViewController : UIViewController,SpecificInterface
 	{
 		private UserBehavior Behavior;
+		private UILabel nbLikeLabel;
+		private UILabel nameLabel;
+		private UIButton start;
+		private UIButton stop;
+		private UIImageView avatarView;
 
 		public HomeViewController () : base ("HomeViewController", null)
 		{
@@ -31,28 +38,61 @@ namespace TAL.iOS
 		{
 			base.ViewDidLoad ();
 
-			this.View.BackgroundColor = UIColor.Blue;
-
-			UIButton button = new UIButton (new RectangleF (10, 20, 100, 20));
-			button.BackgroundColor = UIColor.Yellow;
-			button.TouchUpInside += (o,s) => {
+			start = new UIButton (new RectangleF (10, 20, 100, 30));
+			start.BackgroundColor = UIColor.Yellow;
+			start.SetTitle ("Start !",UIControlState.Normal);
+			start.SetTitleColor (UIColor.Black,UIControlState.Normal);
+			start.TouchUpInside += (o,s) => {
 				this.Behavior.StartAutoLiking ();
 			};
-			this.Add (button);
-			
-			// Perform any additional setup after loading the view, typically from a nib.
+			this.Add (start);
+
+			stop = new UIButton (new RectangleF (200, 20, 100, 30));
+			stop.BackgroundColor = UIColor.Yellow;
+			stop.SetTitle ("Stop !",UIControlState.Normal);
+			stop.SetTitleColor (UIColor.Black,UIControlState.Normal);
+			stop.TouchUpInside += (o,s) => {
+				this.Behavior.StopAutoLiking ();
+			};
+			this.Add (stop);
+
+			nameLabel = new UILabel (new RectangleF (10, 50, 300, 20));
+			nameLabel.TextAlignment = UITextAlignment.Center;
+			this.Add (nameLabel);
+
+			nbLikeLabel = new UILabel (new RectangleF (10, 90, 300, 20));
+			nbLikeLabel.TextAlignment = UITextAlignment.Center;
+			this.Add (nbLikeLabel);
+
+			avatarView = new UIImageView(new RectangleF (10, 130, 300, 300));
+			this.Add (avatarView);
 		}
 
 		public void SetNumberOfLikes(string numberOfLikes) {
-			Console.WriteLine("Number of like : "+numberOfLikes);
+			nbLikeLabel.Text = numberOfLikes;
 		}
 
-		public void AddNewProfile(string profilePicture, string name) {
-			Console.WriteLine("Name : "+name);
+		public async void AddNewProfile(string profilePicture, string name) {
+			Console.WriteLine (name + "( " + profilePicture + " )");
+			nameLabel.Text = name;
+			avatarView.Image = await this.LoadImage (profilePicture);
 		}
 
 		public void SetBehavior(UserBehavior behavior) {
 			this.Behavior = behavior;
+		}
+
+		public async Task<UIImage> LoadImage (string imageUrl)
+		{
+			var httpClient = new HttpClient();
+
+			Task<byte[]> contentsTask = httpClient.GetByteArrayAsync (imageUrl);
+
+			// await! control returns to the caller and the task continues to run on another thread
+			var contents = await contentsTask;
+
+			// load from bytes
+			return UIImage.LoadFromData (NSData.FromArray (contents));
 		}
 	}
 }
